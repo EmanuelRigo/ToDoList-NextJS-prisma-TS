7896f2b9d7b940e8f0ef893fca34c59f6f8da356
Emanuel Rigo <112441019+EmanuelRigo@users.noreply.github.com>
2024-05-29 19:14:45 -0300

    agregando archivos

---
+++ src/app/new/page.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import { useEffect } from "react";
+
+function NewPage({ params }: { params: { id: String } }) {
+  const { handleSubmit, register, setValue } = useForm();
+  const router = useRouter();
+
+  console.log(params);
+
+  useEffect(() => {
+    if (params.id) {
+      axios.get(`/api/tasks/${params.id}`).then((res) => {
+        setValue("title", res.data.title);
+        setValue("description", res.data.description);
+      });
+    }
+  }, []);
+
+  const onSubmit = handleSubmit(async (data) => {
+    if (params.id) {
+      await axios.put(`/api/tasks/${params.id}`, data);
+    } else {
+      await axios.post("/api/tasks", data);
+    }
+    router.push("/");
+    router.refresh();
+  });
+
+  return (
+    <section className="h-[calc(100vh-7rem)] flex items-center justify-center">
+      <form onSubmit={onSubmit} className="w-2/4">
+        <h1 className="text-3xl font-bold mb-5">
+          {params.id ? "Update" : "Create"} Task
+        </h1>
+        <label htmlFor="title" className="font-bold text-xs">
+          Write your title:
+        </label>
+        <input
+          id="title"
+          type="text"
+          placeholder="Write a title"
+          className="px-3 py-1 border border-gray-300 rounded-md shodow-sm focus:outline-none focus:ring-1 focus:ring-sky-300 focus:border-sky-300 text-black block mb-2 w-full"
+          {...register("title")}
+        />
+        <label htmlFor="description" className="font-bold text-xs">
+          Write your description:
+        </label>
+        <textarea
+          id="description"
+          placeholder="Write a description"
+          className="px-3 py-1 border border-gray-300 rounded-md shodow-sm focus:outline-none focus:ring-1 focus:ring-sky-300 focus:border-sky-300 text-black block w-full"
+          {...register("description")}
+        ></textarea>
+        <div className="flex justify-between">
+          <button
+            type="submit"
+            className="bg-sky-500 px-3 py-1 rounded-md text-white mt-4"
+          >
+            {params.id ? "Update" : "Create"}
+          </button>
+          <button
+            type="button"
+            className="bg-red-500 px-3 py-1 rounded-md text-white mt-4"
+            onClick={async () => {
+              if (confirm("Are you sure you want to delete this task")) {
+                await axios.delete(`/api/tasks/${params.id}`);
+                router.push("/");
+                router.refresh();
+              }
+            }}
+          >
+            Delete
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+}
+
+export default NewPage;
